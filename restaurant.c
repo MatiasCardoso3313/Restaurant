7894a1743ee4b3a70074a20f6908b02adecf00bb
MatiasCardoso3313 <mcardoso@fi.uba.ar>
2024-09-12 22:34:56 -0300

    Add files via upload

---
+++ restaurant.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include "restaurant.h"
+
+void inicializar_juego(juego_t *juego){
+    juego->cocina.posicion.fil;
+    juego->cocina.posicion.col;
+
+}
+void mostrar_juego(juego_t juego){
+
+}
