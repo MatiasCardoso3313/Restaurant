cd0dbb9469415d182da1cbf66060b3d3a37b5ea1
MatiasCardoso3313 <mcardoso@fi.uba.ar>
2024-09-14 19:15:43 -0300

    Actualizar restaurant.c

--- restaurant.c
+++ restaurant.c
@@ -2,8 +2,23 @@
 #include<stdbool.h>
 #include "restaurant.h"
 
+void inicializar_terreno(char terreno[][], int tope){
+    if (tope==0){
+       for(int fila, fila<  ,fila++)
+          for(int columna, columna<  , columna++)
+             terreno[fila][columna]=' ';
+    }
+}
+
+void reemplazar_elemento(char terreno[][], coordenada_t posicion, char elemento){
+    terreno[posicion.fila][posicion.columna]=elemento;    
+}
+
+void colocar_mesas(){
+}
+
 void inicializar_juego(juego_t *juego){
-    juego->mesas[10];
+    
     
     juego->cocina.posicion.fil;
     juego->cocina.posicion.col;
