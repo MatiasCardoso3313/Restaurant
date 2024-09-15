83b0b59dd9fa9653bf5b46dfbc985e563e2545b4
MatiasCardoso3313 <mcardoso@fi.uba.ar>
2024-09-15 14:33:51 -0300

    Cambios

--- restaurant.c
+++ restaurant.c
@@ -2,15 +2,13 @@
 #include<stdbool.h>
 #include "restaurant.h"
 
-void inicializar_terreno(char terreno[][], int tope){
-    if (tope==0){
-       for(int fila, fila<  ,fila++)
-          for(int columna, columna<  , columna++)
-             terreno[fila][columna]=' ';
-    }
+void inicializar_terreno(char terreno[MAX_FILAS][MAX_COLUMNAS], int tope){
+    for(int fila, fila<MAX_FILAS  ,fila++)
+      for(int columna, columna<MAX_COLUMNAS  , columna++)
+        terreno[fila][columna]=' ';
 }
 
-void reemplazar_elemento(char terreno[][], coordenada_t posicion, char elemento){
+void reemplazar_elemento(char terreno[MAX_FILAS][MAX_COLUMNAS], coordenada_t posicion, char elemento){
     terreno[posicion.fila][posicion.columna]=elemento;    
 }
 
@@ -18,12 +16,9 @@ void colocar_mesas(){
 }
 
 void inicializar_juego(juego_t *juego){
-    
-    
     juego->cocina.posicion.fil;
     juego->cocina.posicion.col;
 
 }
 void mostrar_juego(juego_t juego){
-
 }
