f4539924178f1578a0258e7bddefe6cbf2785511
MatiasCardoso3313 <mcardoso@fi.uba.ar>
2024-09-24 01:15:51 -0300

    Cambio de nombre a constantes en main

--- juego.c
+++ juego.c
@@ -4,9 +4,9 @@
 #include "restaurant.h"
 
 /* ESTADO DE JUEGO (MAIN)*/
-const int JUEGO_EN_CURSO=0;
-const int JUEGO_PERDIDO=-1;
-const int JUEGO_GANADO=1;
+const int JUEGO_EN_CURSO_MAIN=0;
+const int JUEGO_PERDIDO_MAIN=-1;
+const int JUEGO_GANADO_MAIN=1;
 
 int main(){
     srand((unsigned)time(NULL));
@@ -15,17 +15,17 @@ int main(){
     inicializar_juego(&juego);
 
     mostrar_juego(juego);
-    while (estado_juego(juego) == JUEGO_EN_CURSO){
+    while (estado_juego(juego) == JUEGO_EN_CURSO_MAIN){
         char accion=' ';
         printf("HAZ UNA ACCION=>");
         scanf(" %c", &accion);
         system("clear");
         realizar_jugada(&juego,accion);
         mostrar_juego(juego);
-    }if (estado_juego(juego) == JUEGO_GANADO){
+    }if (estado_juego(juego) == JUEGO_GANADO_MAIN){
         system("clear");
         printf("\n\n\n\n\n\n\n\n\n\n\n\n               -|G A N A S T E|-\n\n\n\n\n\n\n\n\n\n\n\n");
-    }else if(estado_juego(juego) == JUEGO_PERDIDO){
+    }else if(estado_juego(juego) == JUEGO_PERDIDO_MAIN){
         system("clear");
         printf("\n\n\n\n\n\n\n\n\n\n\n\n               -|P E R D I S T E|-\n\n\n\n\n\n\n\n\n\n\n\n");
 
